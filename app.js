09347ccacfbf0d814eaea2d1406c9d618f855607
mot25 <76829487+mot25@users.noreply.github.com>
2021-12-22 00:22:21 +0200

    01

--- app.js
+++ app.js
@@ -126,6 +126,7 @@ bot.command("timesheet", async (ctx) => {
         for (const key of arrTimesheet[0][0].Пн) {
           await ctx.reply(key);
         }
+
         await ctx.reply(
           JSON.stringify(Object.keys(arrTimesheet[1][0]))
             .replace(/"/gi, "")
@@ -170,22 +171,169 @@ bot.command("timesheet", async (ctx) => {
 
 bot.on("message", (ctx) => {
   let key = ctx.update.message.text;
-  console.log("key", key);
-  switch (key) {
-    case "пн":
-      break;
-    case "вт":
-      break;
-    case "ср":
-      break;
-    case "чт":
-      break;
-    case "пт":
-      break;
+  const parseTimesheet = async () => {
+    const URL = "http://109.237.0.203:8083/raspisanie/www/cg38.htm";
+    needle.get(URL, function async(err, res) {
+      if (err) throw err;
+      let a = cheerio.load(res.body);
+      let abc = a("tr");
+      let arr = [];
+      let arrTimesheet;
 
-    default:
-      break;
-  }
+      abc.each((i, e) => {
+        if (
+          (i > 11) &
+          (i < 47) &
+          (i !== 18) &
+          (i !== 25) &
+          (i !== 32) &
+          (i !== 39) &
+          (i !== 46)
+        ) {
+          // date 12 19 26 33 40
+          arr.push(a(e).text());
+        }
+      });
+      arrTimesheet = [
+        [
+          {
+            [arr[0].slice(10, 12)]: [
+              arr[0].slice(12, 120),
+              arr[1],
+              arr[2],
+              arr[3],
+              arr[4],
+              arr[5],
+            ],
+          },
+        ],
+        [
+          {
+            [arr[6].slice(10, 12)]: [
+              arr[6].slice(12, 120),
+              arr[7],
+              arr[8],
+              arr[9],
+              arr[10],
+              arr[11],
+            ],
+          },
+        ],
+        [
+          {
+            [arr[12].slice(10, 12)]: [
+              arr[12].slice(12, 120),
+              arr[13],
+              arr[14],
+              arr[15],
+              arr[16],
+              arr[17],
+            ],
+          },
+        ],
+        [
+          {
+            [arr[18].slice(10, 12)]: [
+              arr[18].slice(12, 120),
+              arr[19],
+              arr[20],
+              arr[21],
+              arr[22],
+              arr[23],
+            ],
+          },
+        ],
+        [
+          {
+            [arr[24].slice(10, 12)]: [
+              arr[24].slice(12, 120),
+              arr[25],
+              arr[26],
+              arr[27],
+              arr[28],
+              arr[29],
+            ],
+          },
+        ],
+      ];
+
+      // async function fun(arrTimesheet) {}
+      // fun(arrTimesheet);
+      switch (key) {
+        case "пн":
+          async function fun(arrTimesheet) {
+            await ctx.reply(
+              JSON.stringify(Object.keys(arrTimesheet[0][0]))
+                .replace(/"/gi, "")
+                .slice(1, 3)
+            );
+            for (const key of arrTimesheet[0][0].Пн) {
+              await ctx.reply(key);
+            }
+          }
+          fun(arrTimesheet);
+    
+          break;
+        case "вт":
+          async function fun1(arrTimesheet) {
+            await ctx.reply(
+              JSON.stringify(Object.keys(arrTimesheet[1][0]))
+                .replace(/"/gi, "")
+                .slice(1, 3)
+            );
+            for (const key of arrTimesheet[1][0].Вт) {
+              await ctx.reply(key);
+            }
+          }
+          fun1(arrTimesheet);
+          break;
+        case "ср":
+          async function fun2(arrTimesheet) {
+            await ctx.reply(
+              JSON.stringify(Object.keys(arrTimesheet[2][0]))
+                .replace(/"/gi, "")
+                .slice(1, 3)
+            );
+            for (const key of arrTimesheet[2][0].Ср) {
+              await ctx.reply(key);
+            }
+          }
+          fun2(arrTimesheet);
+          break;
+        case "чт" || "Чт":
+          async function fun3(arrTimesheet) {
+            await ctx.reply(
+              JSON.stringify(Object.keys(arrTimesheet[3][0]))
+                .replace(/"/gi, "")
+                .slice(1, 3)
+            );
+            for (const key of arrTimesheet[3][0].Чт) {
+              await ctx.reply(key);
+            }
+          }
+          fun3(arrTimesheet);
+          break;
+        case "пт":
+          async function fun4(arrTimesheet) {
+            await ctx.reply(
+              JSON.stringify(Object.keys(arrTimesheet[4][0]))
+                .replace(/"/gi, "")
+                .slice(1, 3)
+            );
+            for (const key of arrTimesheet[4][0].Пт) {
+              await ctx.reply(key);
+            }
+          }
+          fun4(arrTimesheet);
+    
+          break;
+    
+        default:
+          break;
+      }
+    });
+  };
+  parseTimesheet();
 });
 
 bot.launch().then(console.log("bot start"));
