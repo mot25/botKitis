3816c29a3d00a060acd8cb1817df8492f25ea5d0
mot25 <76829487+mot25@users.noreply.github.com>
2022-01-04 12:25:14 +0200

    help scene 2

--- lessonscene.js
+++ lessonscene.js
@@ -1,12 +1,17 @@
-const { Telegraf, session, Scenes: {BaseScene, Stage}, Markup } = require("telegraf");
+const {
+  Telegraf,
+  session,
+  Scenes: { BaseScene, Stage },
+  Markup,
+} = require("telegraf");
 
 const bot = new Telegraf("2123698607:AAEINMnN39PUY0tz470a5QSMBj2UziDnqg4");
 
-const nameScene = new BaseScene("nameScane");
+const nameScene = new BaseScene("nameScene");
 
 nameScene.enter((ctx) => ctx.reply("whats your name?"));
 nameScene.on("text", (ctx) => {
-  ctx.state.name = ctx.message.text;
+  ctx.session.name = ctx.message.text;
   return ctx.scene.leave();
 });
 
@@ -15,20 +20,20 @@ nameScene.leave((ctx) => ctx.reply("new name scene save"));
 const ageScene = new BaseScene("ageScene");
 ageScene.enter((ctx) => ctx.reply("whats your age?"));
 ageScene.on("text", (ctx) => {
-  ctx.state.name = ctx.message.text;
+  ctx.session.name = ctx.message.text;
   return ctx.scene.leave();
 });
 
 ageScene.leave((ctx) => ctx.reply("new ageScene scene save"));
 
-const stage = new Stage([ nameScene, ageScene ]);
+const stage = new Stage([nameScene, ageScene]);
 
 bot.use(session());
 bot.use(stage.middleware());
 bot.command("/start", (ctx) =>
-  ctx.reply(`yor name ${ctx.state.name} and age ${ctx.state.age}`)
+  ctx.reply(`yor name ${ctx.session.name} and age ${ctx.session.age}`)
 );
-bot.command("/name", (ctx) => ctx.scene.enter("nameScene"));
+bot.command("name", (ctx) => ctx.scene.enter("nameScene"));
 bot.command("/age", (ctx) => ctx.scene.enter("ageScene"));
 
 bot.launch().then(console.log("start"));
---
+++ lessonscene2.js
@@ -0,0 +1,45 @@
+const {
+  Telegraf,
+  session,
+  Scenes: { BaseScene, Stage },
+  Markup,
+} = require("telegraf");
+
+const bot = new Telegraf("2123698607:AAEINMnN39PUY0tz470a5QSMBj2UziDnqg4");
+
+const nameScene = new BaseScene("nameScene");
+const ageScene = new BaseScene("ageScene");
+
+nameScene.enter((ctx) => ctx.reply("whats your name"));
+nameScene.on("text", (ctx) => {
+  ctx.session.name = ctx.message.text;
+  ctx.reply('save name')
+  ctx.scene.leave();
+});
+nameScene.leave((ctx) => {
+  ctx.scene.enter("ageScene");
+});
+ageScene.enter((ctx) => {
+  ctx.reply("how many yars");
+});
+ageScene.on("text", async (ctx) => {
+  ctx.session.age = ctx.message.text;
+  ctx.reply('save age')
+  ctx.scene.leave();
+});
+ageScene.leave((ctx) => {
+  ctx.reply("press /readyprofile");
+});
+
+const stage = new Stage([nameScene, ageScene]);
+bot.use(session());
+bot.use(stage.middleware());
+
+bot.command("profile", (ctx) => {
+  ctx.scene.enter("nameScene");
+});
+bot.command("readyprofile", (ctx) => {
+  ctx.reply(`${ctx.session.name} and ${ctx.session.age}`);
+});
+
+bot.launch().then(console.log("start"));
