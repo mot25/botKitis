3e4ef117c7425f87d44bb1b5242decd643fa3be0
mot25 <76829487+mot25@users.noreply.github.com>
2022-01-04 21:27:45 +0200

    sql ready

--- lessonscene.js
+++ lessonscene.js
@@ -1,44 +1,110 @@
 const {
   Telegraf,
   session,
-  Scenes: { BaseScene, Stage },
+  Scenes: { BaseScene, WizardScene, Stage },
   Markup,
 } = require("telegraf");
-
+const fs = require("fs");
+const mysql = require("mysql");
 const bot = new Telegraf("2123698607:AAEINMnN39PUY0tz470a5QSMBj2UziDnqg4");
 
 const keyExit = Markup.keyboard(["exit"]).oneTime().resize();
 const removeKeyboard = Markup.removeKeyboard();
 
-const nameScene = new BaseScene("nameScene");
-nameScene.enter((ctx) => ctx.reply("whats your name?", keyExit));
-nameScene.on("text", (ctx) => {
-  ctx.reply("new name scene save", removeKeyboard);
-  ctx.scene.enter("ageScene", { name: ctx.message.text });
-  return ctx.scene.leave();
+const nameHendler = Telegraf.on("text", async (ctx) => {
+  ctx.session.name = ctx.message.text;
+  await ctx.reply("end scene 1", keyExit);
+  return ctx.wizard.next();
 });
-nameScene.leave((ctx) => ctx.reply("exit scene1"));
 
-const ageScene = new BaseScene("ageScene");
-ageScene.enter((ctx) => ctx.reply("whats your age?", keyExit));
-ageScene.on("text", (ctx) => {
-  ctx.session.name = ctx.scene.state.name;
+const ageHandler = Telegraf.on("text", async (ctx) => {
   ctx.session.age = ctx.message.text;
-  ctx.reply("new ageScene scene save", removeKeyboard);
-  return ctx.scene.leave();
-});
-ageScene.leave((ctx) => {
-  ctx.reply("exit scene2");
-  ctx.reply(`yor name ${ctx.session.name} and age ${ctx.session.age}`);
+  await ctx.reply("end scene 2", removeKeyboard);
+  ctx.scene.leave();
 });
 
-const stage = new Stage([nameScene, ageScene]);
+const infoScene = new WizardScene("infoScene", nameHendler, ageHandler);
+infoScene.enter((ctx) => ctx.reply("enter wizardScence", keyExit));
+infoScene.leave(async (ctx) => {
+  await fs.writeFileSync(
+    "txt.txt",
+    `yor name ${ctx.session.name} \n and\n age ${ctx.session.age}`
+  );
+  const conn = mysql.createConnection({
+    host: "127.0.0.5",
+    port: 3306,
+    user: "root",
+    database: "users",
+    password: "root",
+  });
+  await conn.connect((err) => {
+    if (err) {
+      return console.log(err);
+    } else {
+      console.log("DB--ok");
+    }
+  });
+  let queryInsert = "INSERT INTO users.new_table2 (`name`, `nametg`, `login`, `password`, `group`, `popular`, `love`) VALUES ('"+ ctx.session.name +"', 'rrrrr', 'rrrr', '"+ ctx.session.age +"', 'rrrrrr', '96', 'rrrr')";
+  // let queryInsert = `insert  \`new_table\` (\`name\`, \`password\`) values (${ctx.session.name},  ${ctx.session.age}`;
+  let query = "select * from new_table2";
+  await conn.query(queryInsert, (err, res) => {
+    if (err) {
+      return console.log(err);
+    }
+    console.log(res);
+  });
+  await conn.end(async (err) => {
+    if (err) {
+      return console.log(err);
+    } else {
+      console.log("DB--end");
+      await ctx.reply("BD");
+      await ctx.reply("👌");
+    }
+  });
+  await ctx.reply("👌");
+});
+const stage = new Stage([infoScene]);
 stage.hears("exit", (ctx) => ctx.scene.leave());
 bot.use(session());
 bot.use(stage.middleware());
 bot.command("/start", (ctx) => {
-  ctx.reply(`yor name ${ctx.session.name} and age ${ctx.session.age}`);
+  ctx.reply(`yor name ${ctx.session.name} \n and\n age ${ctx.session.age}`);
 });
-bot.command("name", (ctx) => ctx.scene.enter("nameScene"));
-bot.command("/age", (ctx) => ctx.scene.enter("ageScene"));
+bot.command("bd", async (ctx) => {
+  const conn = mysql.createConnection({
+    host: "127.0.0.5",
+    port: 3306,
+    user: "root",
+    database: "users",
+    password: "root",
+  });
+  await conn.connect((err) => {
+    if (err) {
+      return console.log(err);
+    } else {
+      console.log("DB--ok");
+    }
+  });
+  let queryInsert = "INSERT INTO users.new_table2 (`name`, `nametg`, `login`, `password`, `group`, `popular`, `love`) VALUES ('"+ 'proverka' +"', 'rrrrr', 'rrrr', 'rrrr', 'rrrrrr', '96', 'rrrr')";
+  // let queryInsert = `insert  \`new_table\` (\`name\`, \`password\`) values (${ctx.session.name},  ${ctx.session.age}`;
+  let query = "select * from new_table2";
+  await conn.query(queryInsert, (err, res) => {
+    if (err) {
+      return console.log(err);
+    }
+    console.log(res);
+  });
+  await conn.end(async (err) => {
+    if (err) {
+      return console.log(err);
+    } else {
+      console.log("DB--end");
+      await ctx.reply("BD");
+      await ctx.reply("👌");
+    }
+  });
+});
+bot.command("name", (ctx) => ctx.scene.enter("infoScene"));
+bot.command("log", (ctx) => console.log(ctx.session));
 bot.launch().then(console.log("start"));
